ed4506114e67c73b064bd42750a0347f0cd2e617
marco <marco@marco-desktop>
2012-11-13 01:54:37 +0100

    some changes

--- site/views/gallery/tmpl/default.php
+++ site/views/gallery/tmpl/default.php
@@ -1,6 +1,18 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 ?>
+<h2><?php echo $this->title; ?></h2>
+<ul>
+	<?php foreach($this->folders as $folder) { ?>
+		<li>
+			<a href="<?php echo JRoute::_('index.php?option=com_gallery&path=' . $folder->path); ?>">
+				<img src="<?php echo $folder->previewPhoto; ?>" />
+			</a>
+		</li>
+	<?php } ?>
+
+</ul>
+
 <ul>
 	<?php foreach($this->photos as $photo) { ?>
 		<li><img src="<?php echo $photo; ?>" /></li>
--- site/views/gallery/view.html.php
+++ site/views/gallery/view.html.php
@@ -3,72 +3,111 @@ defined('_JEXEC') or die( 'Restricted Access' );
 jimport('joomla.application.component.view');
 
 class GalleryViewGallery extends JView
-{ 
+{
+	var $photoWidth  = 220;
+	var $photoHeight = 220;
+	
 	function display($tpl = null)
 	{
 		// params // TODO auslagern
 		$galleryBasePath 	= JPATH_BASE . DS . 'images' . DS . 'gallery';
 		$photosBasePath 	= $galleryBasePath . DS . 'photos';
 		$thumbnailsBasePath = $galleryBasePath . DS . 'thumbnails';
+		// ---
 		
-		$photosBasePath 	.= DS . '2012'; // TODO remove
-		$thumbnailsBasePath .= DS . '2012'; // TODO remove
+		// get path from GET
+		$basePath = JRequest::getString('path');
+		$basePath = JFolder::makeSafe($basePath);
 		
-		$photoWidth  = 220;
-		$photoHeight = 220;
+		if ($basePath != '') {
+			$photosPath		= $photosBasePath . DS . $basePath;
+			$thumbnailsPath = $thumbnailsBasePath . DS . $basePath;
+		}
+		else {
+			$photosPath		= $photosBasePath;
+			$thumbnailsPath = $thumbnailsBasePath;
+		}
 		// ---
 		
 		// get folders
-		$folders = JFolder::folders($photosBasePath);
+		$folders = JFolder::folders($photosPath);
 		
 		// get random folder preview photos
 		$previewPhotos = array();
 		
-		foreach($folders as $folder) {
+		for ($i = 0; $i < count($folders); $i++) {
+			
+			$folder = $folders[$i];
 			
 			// define folders
-			$photosBasePathFolder = $photosBasePath . DS . $folder;
-			$thumbnailsBasePathFolder = $thumbnailsBasePath . DS . $folder;
+			$photosBasePathFolder = $photosPath . DS . $folder;
+			$thumbnailsBasePathFolder = $thumbnailsPath . DS . $folder;
 			
 			// get files in folders
-			$files = JFolder::files($photosBasePathFolder);
-			$filesCount = count($files);
+			$filepaths = JFolder::files($photosBasePathFolder, '.', true, true);
+			$filesCount = count($filepaths);
 			
-			// calculate index of preview photo
-			$photoIndex = rand(0, $filesCount - 1);
-			$photoPath = $photosBasePathFolder . DS . $files[$photoIndex];
+			// check if a image is available
+			if ($filesCount > 0) {
 			
-			// define thumbnail file path
-			$thumbnailFilePath = $thumbnailsBasePathFolder . DS . $files[$photoIndex];
-			
-			// check if thumbnail already exists and create it if not
-			if (!JFile::exists($thumbnailFilePath)) {
+				// calculate index of preview photo
+				$photoIndex = rand(0, $filesCount - 1);
+				$photoPath = $filepaths[$photoIndex];
 				
-				// resize image
-				$photo = new JImage($photoPath);
-				$thumbnail = $photo->resize($photoWidth, $photoHeight, true, 3);
+				$thumbnailFilePath = self::getThumbnailFilePath($photosPath, $thumbnailsPath, $photoPath, $thumbnailsBasePathFolder);
 				
-				// crop image
-				$offsetLeft = ($thumbnail->getWidth() - $photoWidth) / 2;
-				$offsetTop = ($thumbnail->getHeight() - $photoHeight) / 2;
-				$thumbnail->crop($photoWidth, $photoHeight, $offsetLeft, $offsetTop, false);
+				// create folder object for view and add to list
+				unset($tmp);
+				$tmp->path = str_replace($thumbnailsBasePath . DS, '', $thumbnailsBasePathFolder);
+				$tmp->previewPhoto = str_replace(JPATH_BASE . DS, '', $thumbnailFilePath); // remove file path
 				
-				// create folders (recursive) and write file
-				if (JFolder::create($thumbnailsBasePathFolder)) {
-					$thumbnail->toFile($thumbnailFilePath);
-				}
+				$previewPhotos[] = $tmp;
 			}
-			
-			// remove file path and add uri to list
-			$thumbnailURI = str_replace(JPATH_BASE . DS, '', $thumbnailFilePath);
-			$previewPhotos[] = $thumbnailURI;
 		}
 		// ---
 		
+		// get photos of this folder
+		$photos = array();
+		
+		$filepaths = JFolder::files($photosPath, '.', false, true);
+		$filesCount = count($filepaths);
+		
+		for ($i = 0; $i < $filesCount; $i++) {
+			$thumbnailFilePath = self::getThumbnailFilePath($photosPath, $thumbnailsPath, $filepaths[$i], $thumbnailsBasePathFolder);
+			$photos[] = str_replace(JPATH_BASE . DS, '', $thumbnailFilePath);
+		}
+		
 		// assign Variables
-		$this->assignRef('photos', $previewPhotos);
+		$this->assignRef('folders', $previewPhotos); // TODO rename previewPhotos
+		$this->assignRef('photos', $photos);
+		//$this->assignRef('title', ''); // TODO
 		
 		parent::display($tpl);
+	}
+	
+	function getThumbnailFilePath($photosPath, $thumbnailsPath, $photoPath, $thumbnailsBasePathFolder) {
+		// define thumbnail file path
+		$thumbnailFilePath = str_replace($photosPath, $thumbnailsPath, $photoPath);
+		
+		// check if thumbnail already exists and create it if not
+		if (!JFile::exists($thumbnailFilePath)) {
+			
+			// resize image
+			$photo = new JImage($photoPath);
+			$thumbnail = $photo->resize($this->photoWidth, $this->photoHeight, true, 3);
+			
+			// crop image
+			$offsetLeft = ($thumbnail->getWidth() - $this->photoWidth) / 2;
+			$offsetTop = ($thumbnail->getHeight() - $this->photoHeight) / 2;
+			$thumbnail->crop($this->photoWidth, $this->photoHeight, $offsetLeft, $offsetTop, false);
+			
+			// create folders (recursive) and write file
+			if (JFolder::create($thumbnailsBasePathFolder)) {
+				$thumbnail->toFile($thumbnailFilePath);
+			}
+		}
+		
+		return $thumbnailFilePath;
 	}
 }
 ?>
