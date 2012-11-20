475b197707a6674525a2173aacb8812db01649f7
marco <marco@marco-desktop>
2012-11-20 02:15:14 +0100

    shutter added

--- site/views/gallery/tmpl/default.php
+++ site/views/gallery/tmpl/default.php
@@ -1,32 +1,37 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 ?>
-<h2><?php // echo $this->title; ?></h2>
-<ul>
-	<?php
-	foreach($this->childFolders as $childFolder) {
-
-		$previewPhoto = $childFolder->getPreviewPhoto();
-		if ($previewPhoto != null) { // TODO in view.html
-	?>
-			<li>
-				<a href="<?php echo JRoute::_('index.php?option=com_gallery&path=' . $childFolder->getFolderPath()); ?>">
-					<img src="<?php  echo $previewPhoto->getThumbnailURL(); ?>" />
-				</a>
-			</li>
-	<?php
+<div id="gallery">
+	<h1><?php // echo $this->title; ?></h1>
+	<h2>Folders</h2>
+	<div id="folders" class="container">
+		<?php
+		foreach($this->childFolders as $childFolder) {
+	
+			$previewPhoto = $childFolder->getPreviewPhoto();
+			if ($previewPhoto != null) { // TODO in view.html
+		?>
+				<div class="gallery_item">
+					<a href="<?php echo JRoute::_('index.php?option=com_gallery&path=' . $childFolder->getFolderPath()); ?>">
+						<img src="<?php  echo $previewPhoto->getThumbnailURL(); ?>" />
+					</a>
+				</div>
+		<?php
+			}
 		}
-	}
-	?>
-
-</ul>
-
-<ul>
-	<?php foreach($this->photos as $photo) { ?>
-		<li>
-			<a href="<?php echo JRoute::_('index.php?option=com_gallery&view=photo&path=' . $photo->getFolder()->getFolderPath() . '&filename=' . $photo->getFilename()); ?>">
-				<img src="<?php echo $photo->getThumbnailURL(); ?>" />
-			</a>
-		</li>
-	<?php } ?>
-</ul>
+		?>
+	
+	</div>
+	
+	<h2>Photos</h2>
+	<div id="photos" class="container">
+		<?php foreach($this->photos as $photo) { ?>
+			<div class="gallery_item">
+				<a class="shutterset" href="<?php echo $photo->getResizedURL(); ?>">
+				<!--<a class="shutter" href="<?php echo JRoute::_('index.php?option=com_gallery&view=photo&path=' . $photo->getFolder()->getFolderPath() . '&filename=' . $photo->getFilename()); ?>">-->
+					<img src="<?php echo $photo->getThumbnailURL(); ?>" />
+				</a>
+			</div>
+		<?php } ?>
+	</div>
+</div>
