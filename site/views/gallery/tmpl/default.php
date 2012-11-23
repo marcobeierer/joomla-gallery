80eb1bc170790fd543451122fd1514c4002f49ae
marco <marco@marco-desktop>
2012-11-23 01:08:42 +0100

    first working version

--- site/views/gallery/tmpl/default.php
+++ site/views/gallery/tmpl/default.php
@@ -2,27 +2,18 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 <div id="gallery">
-	<h1><?php echo $this->title; ?></h1>
-	
 	<?php if (count($this->childFolders) > 0) { ?>
-	<h2>Folders</h2>
-	
 	<div id="folders" class="container">
 		<?php foreach($this->childFolders as $childFolder) { ?>
-			<?php if ($childFolder->getPreviewPhoto() != null) { // TODO in view.html ?>
 			<div class="gallery_item">
 				<a href="<?php echo JRoute::_('index.php?option=com_gallery&path=' . $childFolder->getFolderPath()); ?>">
-					<img class="caption" alt="<?php echo $childFolder->getFolderName(); ?>" src="<?php  echo $childFolder->getPreviewPhoto()->getThumbnailURL(); ?>" />
+					<img class="caption" alt="<?php echo $childFolder->getReadableFolderName(); ?>" src="<?php  echo $childFolder->getPreviewPhoto()->getThumbnailURL(); ?>" />
 				</a>
 			</div>
-			<?php } ?>
 		<?php } ?>	
+		<div class="clear"></div>
 	</div>
-	<?php } ?>
-	
-	<?php if (count($this->photos) > 0) { ?>
-	<h2>Photos</h2>
-	
+	<?php } else if (count($this->photos) > 0) { ?>
 	<div id="photos" class="container">
 		<?php foreach($this->photos as $photo) { ?>
 			<div class="gallery_item">
@@ -32,6 +23,7 @@ defined('_JEXEC') or die('Restricted access');
 				</a>
 			</div>
 		<?php } ?>
+		<div class="clear"></div>
 	</div>
 	<?php } ?>
 </div>
