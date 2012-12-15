c9eab9ae0fab28641f471d70853c12f82260b493
marco <marco@marco-desktop>
2012-12-15 02:04:49 +0100

    random redirect after login

--- site/views/galleries/tmpl/default.php
+++ site/views/galleries/tmpl/default.php
@@ -1,16 +1,3 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 ?>
-<div id="gallery">
-	<div id="folders" class="container">
-		<?php foreach($this->galleryFolders as $galleryFolder) { ?>
-			<?php JFactory::getApplication()->getParams()->set('gallery_path', $galleryFolder->menuItemParams->get('gallery_path')); // TODO dont do this at home // bad hack ?>
-			<div class="gallery_item">
-				<a href="<?php  echo JRoute::_('index.php?option=com_gallery&view=gallery&Itemid=' . $galleryFolder->menuItem->id); ?>">
-					<img class="caption" alt="<?php echo $galleryFolder->menuItem->title; ?>" src="<?php echo $galleryFolder->getPreviewPhoto()->getThumbnailURL(); ?>" />
-				</a>
-			</div>
-		<?php } ?>	
-		<div class="clear"></div>
-	</div>
-</div>
--- site/views/galleries/view.html.php
+++ site/views/galleries/view.html.php
@@ -15,44 +15,9 @@ class GalleryViewGalleries extends JView
 		$menuItems = $menu->getItems('menutype', 'photoalben'); // TODO variabel als param
 		// ---
 		
-		$galleryFolders = array();
-		foreach ($menuItems as $menuItem) {
-			
-			$menuItemParams = new JParameter($menuItem->params);
-			$galleryPath = JPATH_BASE . DS . $menuItemParams->get('gallery_path'); // TODO validate
-			
-			$folder = new Folder($galleryPath, '/');
-			$folder->menuItem = $menuItem; // TODO don't do such ugly stuff
-			$folder->menuItemParams = $menuItemParams;
-			
-			$galleryFolders[] = $folder;
-		}
+		$selectedGallery = $menuItems[rand(0, count($menuItems) - 1)];
 		
-		// TODO add scripts and css files in a separate file, so that it isn't necessary to add all stuff in every view
-		
-		// add scripts
-		$document = &JFactory::getDocument();
-		
-		$document->addScript('media/com_gallery/js/jquery-1.8.3.min.js');
-		$document->addScript('media/com_gallery/js/jquery.capty.min.js');
-		
-		$document->addScriptDeclaration('
-			$(function(){
-				$(\'#gallery .caption\').capty({
-					animation: \'fade\',
-					speed: 400
-				});
-			});
-		');
-		// ---
-		
-		// add css
-		$document->addStyleSheet('media/com_gallery/css/gallery.style.css');
-		$document->addStyleSheet('media/com_gallery/css/jquery.capty.css');
-		
-		$this->assignRef('galleryFolders', $galleryFolders);
-		parent::display($tpl);
+		$application->redirect(JRoute::_($selectedGallery->link . '&Itemid=' . $selectedGallery->id));
 	}
-	
 }
 ?>
