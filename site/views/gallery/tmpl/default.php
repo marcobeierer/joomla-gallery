5f51ffd99a5a80ca9ca158ba0d3b93f195b05466
marco <marco@marco-desktop>
2013-01-06 15:19:05 +0100

    some changes

--- site/views/gallery/tmpl/default.php
+++ site/views/gallery/tmpl/default.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <div id="gallery">
 	<?php if (count($this->childFolders) > 0) { ?>
-	<div id="folders" class="container">
+	<div id="folders">
 		<?php foreach($this->childFolders as $childFolder) { ?>
 			<div class="gallery_item">
 				<a href="<?php  echo JRoute::_('index.php?option=com_gallery&path=' . $childFolder->getFolderPath()); ?>">
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('Restricted access');
 		<div class="clear"></div>
 	</div>
 	<?php } else if (count($this->photos) > 0) { ?>
-	<div id="photos" class="container">
+	<div id="photos">
 		<?php foreach($this->photos as $photo) { ?>
 			<div class="gallery_item">
 				<a class="shutterset" href="<?php echo $photo->getResizedURL(); ?>">
