0f0a15868373e53ea713f97d889dce7a5dcf21cf
marco <marco@marco-desktop>
2013-01-15 03:32:49 +0100

    module navigation fertiggestellt

--- modules/mod_gallery_navigation/tmpl/default.php
+++ modules/mod_gallery_navigation/tmpl/default.php
@@ -2,16 +2,22 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 
-<div id="mod_gallery_navigation">
-<?php if ($previousFolder != '') { ?>
-	<div class="previous">
-		<?php echo JText::_('Previous Folder'); ?>: 
-		<a href="<?php echo $hrefPreviousFolder; ?>"><?php echo $previousFolder; ?></a>
+<div id="mod_gallery_navigation" class="row-fluid">
+	<div class="previous span5">
+		<?php if ($previousFolder != '') { ?>
+			<?php echo JText::_('PREVIOUS_FOLDER'); ?>: 
+			<a href="<?php echo $hrefPreviousFolder; ?>"><?php echo $previousFolder; ?></a>
+		<?php } ?>
 	</div>
-<?php } if ($nextFolder != '') { ?>
-	<div class="next pull-right">
-		<?php echo JText::_('Next Folder'); ?>:
-		<a href="<?php echo $hrefNextFolder; ?>"><?php echo $nextFolder; ?></a>
+	<div class="up span2">
+		<?php if ($oneLayerUp) { ?>
+			<a href="<?php echo $hrefOneLayerUp; ?>"><?php echo JText::_('ONE_LAYER_UP'); ?></a>
+		<?php } ?>
 	</div>
-<?php } ?>
+	<div class="next span5">
+		<?php if ($nextFolder != '') { ?>
+			<?php echo JText::_('NEXT_FOLDER'); ?>:
+			<a href="<?php echo $hrefNextFolder; ?>"><?php echo $nextFolder; ?></a>
+		<?php } ?>
+	</div>
 </div>
