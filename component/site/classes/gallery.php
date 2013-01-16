abbcf41db82ea45564432cdcf9358b543d4787f8
marco <marco@marco-desktop>
2013-01-17 00:01:01 +0100

    refactoring gallery.php

---
+++ component/site/classes/gallery.php
@@ -0,0 +1,117 @@
+<?php
+defined('_JEXEC') or die('Restricted Access');
+
+class Gallery extends JObject { 
+	
+	private $galleryPath;
+	private $currentRequestPath;
+	private $currentRequestFilename;
+	
+	function __construct($params) {
+		
+		/* check if valid page (gallery_path isset) */
+		if (JRequest::getWord('view') == 'gallery' && $params->get('gallery_path', '') == '') {
+			JError::raiseError(404, JText::_("Page Not Found")); exit;
+		}
+		/* --- */
+		
+		$this->loadSafeRequestVars();
+		$this->makeParamsSafe($params);
+	}
+	
+	private function makeParamsSafe($params) {
+		
+		// TODO check if absolute path is used as gallery_path in settings and add JPATH_BASE just if relative
+		
+		$this->galleryPath = JPATH_BASE . DS . JFolder::makeSafe($params->get('gallery_path')); // TODO safe enough?
+		$params->set('gallery_path', $this->galleryPath); // for legacy use
+	}
+	
+	private function loadSafeRequestVars() {
+		
+		if (JRequest::getVar('view') == 'file') {
+			$pathObject = GalleryHelper::splitPath(JRequest::getString('path', ''));
+			
+			$this->currentRequestPath = JFolder::makeSafe($pathObject->folderPath);
+			$this->currentRequestFilename = JFile::makeSafe($pathObject->filename);
+		}
+		else {
+			$this->currentRequestPath = JFolder::makeSafe(JRequest::getString('path', ''));
+			$this->currentRequestFilename = '';
+		}
+	}
+	
+	public function getRequestPathWithFilename() {
+		return $this->galleryPath . DS . $this->currentRequestPath . DS . $this->currentRequestFilename;
+	}
+		
+	public function getPhotosPath() {
+		return $this->galleryPath . DS . 'photos';
+	}
+	
+	/* check if path is valid and raise error otherwise */
+	public function validateRequestPath() { 
+		
+		if (JRequest::getVar('view') == 'file') { // TODO cleaner
+			$fullPath = $this->galleryPath . DS . $this->currentRequestPath;
+		} else {
+			$fullPath = $this->galleryPath . DS . 'photos' . DS . $this->currentRequestPath;
+		}
+		
+		if ($this->currentRequestPath != '' && !(JFolder::exists($fullPath) || JFile::exists($fullPath))) {
+			JError::raiseError(404, JText::_("Page Not Found")); exit;
+		}
+	}
+	
+	/* create htaccess file if it not already exists */
+	public function createHtaccessFile() {
+		
+		$htaccessPath = $this->galleryPath . DS . '.htaccess';
+		
+		if (!JFile::exists($htaccessPath)) { // TODO error handling
+			
+			$htaccessContent = "deny from all\n";
+			JFile::write($htaccessPath, $htaccessContent);
+		}
+	}
+	
+	/* create photos directory if it not already exists */
+	public function createInitialDirectories() {
+		
+		if (!JFolder::exists($this->getPhotosPath())) { // TODO error handling
+			JFolder::create($this->getPhotosPath());
+		}
+	}
+	
+	public function handleFileRequests() {
+
+		if (JRequest::getVar('view') == 'file') {
+
+			$filepath = $this->getRequestPathWithFilename();
+			if (file_exists($filepath)) {
+				
+				// open the file in a binary mode
+				$fp = fopen($filepath, 'rb');
+				
+				// send the right headers
+				header('Accept-Ranges: bytes');
+				header('Content-Length: ' . filesize($filepath));
+				header('Content-Type: ' . mime_content_type($filepath));
+				//header('Cache-Control: public, min-fresh=3600, max-age=3600, s-maxage=3600, must-revalidate');
+				
+				// dump the picture and stop the script
+				fpassthru($fp);
+				fclose($fp);
+			}
+			exit;
+		}
+	}
+	
+	public function setModuleParams() {
+		
+		JRequest::setVar('is_gallery', true);
+		JRequest::setVar('current_path', $this->getPhotosPath() . DS . $this->currentRequestPath);
+		JRequest::setVar('photos_path', $this->getPhotosPath());
+	}
+	
+}
--- component/site/gallery.php
+++ component/site/gallery.php
@@ -1,91 +1,25 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-// TODO use spl autoload
-require_once (JPATH_COMPONENT . DS . 'helpers' . DS . 'helper.php');
+/* include files (TODO use spl autoload) */ 
+require_once(JPATH_COMPONENT . DS . 'controller.php');
+require_once(JPATH_COMPONENT . DS . 'helpers' . DS . 'helper.php');
 
-// Params validation // TODO at this place or when saved in backend?
-$params = JFactory::getApplication()->getParams();
-
-if (JRequest::getWord('view') == 'gallery' && $params->get('gallery_path', '') == '') { // check if valid page (gallery_path isset)
-	JError::raiseError(404, JText::_("Page Not Found")); exit;
-}
-// TODO check if absolute path is used as gallery_path in settings and add JPATH_BASE just if relative
-$params->set('gallery_path', JPATH_BASE . DS . JFolder::makeSafe($params->get('gallery_path'))); // TODO safe enough?
-// ---
-
-// check if path is valid and raise error otherwise // TODO cleaner
-$path = JRequest::getString('path', '');
-if (JRequest::getVar('view') == 'file') {
-	$fullPath = $params->get('gallery_path', '') . DS . $path;
-} else {
-	$fullPath = $params->get('gallery_path', '') . DS . 'photos' . DS . $path;
-}
-
-if ($path != '' && !(JFolder::exists($fullPath) || JFile::exists($fullPath))) {
-	JError::raiseError(404, JText::_("Page Not Found")); exit;
-}
-// ---
-
-// create htaccess file if it not already exists // TODO necessary with every call?
-$htaccessPath = $params->get('gallery_path') . DS . '.htaccess';
-
-if (!JFile::exists($htaccessPath)) {
-	
-	$htaccessContent = "deny from all\n";
-	JFile::write($htaccessPath, $htaccessContent);
-}
-// TODO error handling
-// ---
-
-// create photos directory if it not already exists
-$photosPath = $params->get('gallery_path') . DS . 'photos';
-
-if (!JFolder::exists($photosPath)) {
-	JFolder::create($photosPath);
-}
-// TODO error handling
-// ---
-
-// handle file requests
-if (JRequest::getVar('view') == 'file') {
-	
-	$pathObject = GalleryHelper::splitPath(JRequest::getString('path', ''));
-	
-	$path = $params->get('gallery_path') . DS . $pathObject->folderPath;
-	$filename = $pathObject->filename;
-		
-	$filepath = $path . DS . $filename;
-	if (file_exists($filepath)) {
-		
-		// open the file in a binary mode
-		$fp = fopen($filepath, 'rb');
-		
-		// send the right headers
-		header('Accept-Ranges: bytes');
-		header('Content-Length: ' . filesize($filepath));
-		header('Content-Type: ' . mime_content_type($filepath));
-		//header('Cache-Control: public, min-fresh=3600, max-age=3600, s-maxage=3600, must-revalidate');
-		
-		// dump the picture and stop the script
-		fpassthru($fp);
-		fclose($fp);
-	}
-	exit;
-}
-// ---
-
-// set params for modules
-JRequest::setVar('is_gallery', true);
-JRequest::setVar('current_path', $photosPath . DS . JFolder::makeSafe(JRequest::getString('path', '')));
-JRequest::setVar('photos_path', $photosPath);
-// ---
-
-require_once (JPATH_COMPONENT . DS . 'controller.php');
-
-// TODO use spl autoload
+require_once(JPATH_COMPONENT . DS . 'classes' . DS . 'gallery.php');
 require_once(JPATH_COMPONENT . DS . 'classes' . DS . 'folder.php');
 require_once(JPATH_COMPONENT . DS . 'classes' . DS . 'photo.php');
+/* --- */
+
+/* initialise gallery */
+$app =& JFactory::getApplication();
+$gallery = new Gallery($app->getParams());
+
+$gallery->validateRequestPath();
+$gallery->createHtaccessFile(); // TODO necessary with every call?
+$gallery->createInitialDirectories();
+$gallery->handleFileRequests();
+$gallery->setModuleParams();
+/* --- */
 
 $classname = 'GalleryController'; 
 $controller = new $classname(); 
